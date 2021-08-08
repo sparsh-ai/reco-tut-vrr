4219f45043047a9c34ad4b01a3f1f05bb1a5d73d
sparsh-ai <62965911+sparsh-ai@users.noreply.github.com>
2021-08-08 12:33:47 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='code',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Recommender System Tutorial',
+    author='recohut',
+    license='MIT',
+)
